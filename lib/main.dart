b3933ee759d161480c448f0e65b2c466504ca9f2
RiTa-23 <rita.tifrt@gmail.com>
2025-03-06 01:23:40 +0900

    add:DBから喫煙所の情報取得＆表示

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'auth_gate.dart';
+import 'package:provider/provider.dart';
+import 'package:mokumou_hazard/model/smoking_area_model.dart';
+import 'package:mokumou_hazard/view_model/smoking_area_view_model.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => SmokingAreaListModel()),
+        ChangeNotifierProxyProvider<SmokingAreaListModel, SmokingAreaViewModel>(
+          create: (context) =>
+              SmokingAreaViewModel(context.read<SmokingAreaListModel>()),
+          update: (context, smokingAreaListModel, smokingAreaViewModel) =>
+              smokingAreaViewModel!..loadSmokingAreas(),
+        ),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -18,11 +34,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+
       home: LoginPage(),
     );
   }
